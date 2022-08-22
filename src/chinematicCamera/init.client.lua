e2cf3c9a92f63130b1c038259ce6c3c5733c46ab
Arda Yildirim <ardayildirim05@icloud.com>
2022-08-22 16:29:04 +0200

    Version: 1

--- src/chinematicCamera/init.client.lua
+++ src/chinematicCamera/init.client.lua
@@ -9,8 +9,6 @@ local camera = Workspace.CurrentCamera
 
 local part = Workspace:WaitForChild("Part")
 
-local count = 1
-
 local function tween(goal)
 	local info = TweenInfo.new(6, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0)
 	local goals = {CFrame = goal}
@@ -21,57 +19,100 @@ local function tween(goal)
 	return tweenObject
 end
 
-local function cinematicCamera(basePart)
-	camera.CameraType = Enum.CameraType.Scriptable
+--[[
+	Data format:
 
+	{
+		extract = {1, 2};
+		insert = {1, 2};
+		cFrames = {
+			start = {CFrame.new()};
+			goal = {CFrame.new()};
+		}
+	}
+]]
+
+local function cinematicCamera(basePart, data)
+	local count = 1
 	local position = basePart.Position
 	local lookVector = basePart.CFrame.LookVector
 	local rightVector = basePart.CFrame.RightVector
 	local upVector = basePart.CFrame.UpVector
 
-	local startPositions = {
-		CFrame.new (
-			position + (lookVector * 15) + (rightVector * 2.5) + (upVector * 5),
-			position + (rightVector * 2.5)
-		);
-		CFrame.new (
-			position + (lookVector * 15) - (Vector3.new(((basePart.Size.X/2) * 2), 0, 0)) + (upVector * 5),
-			basePart.Position
-		);
-		CFrame.new (
-			position + (lookVector * 10),
-			basePart.Position
-		);
-	}
-	
-	local goals = {
-		CFrame.new (
-			position + (lookVector * 15) - (rightVector * 2.5) + (upVector * 5),
-			position - (rightVector * 2.5)
-		);
-
-		CFrame.new (
-			position + (lookVector * -15) - (Vector3.new(((basePart.Size.X/2) * 2), 0, 0)) + (upVector * 5),
-			basePart.Position
-		);
-		CFrame.new (
-			position + (lookVector * 28) + (upVector * 8),
-			basePart.Position
-		);
-	}
-	
-	while (true) do
-		local c = startPositions[count]
-		camera.CFrame = c
-		local t = tween(goals[count])
-		t.Completed:Wait()
-
-		count += 1
-
-		count = if (count > #startPositions) then 1 else count
-
-		-- Fade dispaly dark and wait for fade completition. Use Utility module to implement fade feature
+	local function getStartCFrames()
+		return {
+			CFrame.new (
+				position + (lookVector * 15) + (rightVector * 2.5) + (upVector * 5),
+				position + (rightVector * 2.5)
+			);
+			CFrame.new (
+				position + (lookVector * 15) - (Vector3.new(((basePart.Size.X/2) * 2), 0, 0)) + (upVector * 5),
+				basePart.Position
+			);
+			CFrame.new (
+				position + (lookVector * 10),
+				basePart.Position
+			);
+		}
 	end
+
+	local function getGoalCFrames()
+		return {
+			CFrame.new (
+				position + (lookVector * 15) - (rightVector * 2.5) + (upVector * 5),
+				position - (rightVector * 2.5)
+			);
+	
+			CFrame.new (
+				position + (lookVector * -15) - (Vector3.new(((basePart.Size.X/2) * 2), 0, 0)) + (upVector * 5),
+				basePart.Position
+			);
+			CFrame.new (
+				position + (lookVector * 28) + (upVector * 8),
+				basePart.Position
+			);
+		}
+	end
+
+	local startCFrames = getStartCFrames()
+	local goalCFrames = getGoalCFrames()
+
+	local function handleCFrames()
+		if (not data) then return end
+
+		for _, extractPosition in pairs(data.extract) do
+			table.remove(startCFrames, extractPosition)
+			table.remove(goalCFrames, extractPosition)
+		end
+
+		for index, insertPosition in pairs(data.insert) do
+			local startCFrame = data.cFrames.start[index]
+			local goalCFrame = data.cFrames.goal[index]
+
+			table.insert(startCFrames, startCFrame, insertPosition)
+			table.insert(goalCFrames, goalCFrame, insertPosition)
+		end
+	end
+
+	local function execution()
+		camera.CameraType = Enum.CameraType.Scriptable
+	
+		while (true) do
+			local c = startCFrames[count]
+			camera.CFrame = c
+			local t = tween(goalCFrames[count])
+			t.Completed:Wait()
+
+			count += 1
+
+			count = if (count > #startCFrames) then 1 else count
+
+			-- Fade dispaly dark and wait for fade completition. Use Utility module to implement fade feature
+		end
+	end
+
+	handleCFrames()
+	execution()	
 end
 
 task.wait(3)
